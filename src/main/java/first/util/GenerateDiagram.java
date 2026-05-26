3aef58459dd06bf75a8aed776c3bf407e76533dd
Daniel1464 <Daniel_Chen@caryacademy.org>
2026-05-26 19:30:35 -0400

    Docs update

--- src/main/java/first/util/GenerateDiagram.java
+++ src/main/java/first/util/GenerateDiagram.java
@@ -2,6 +2,10 @@ package first.util;
 
 import java.lang.annotation.Target;
 
+/**
+ * Generates a diagram for a state machine encapsulated within a method.
+ * Note that this doesn't support multiple state machines per method.
+ */
 @Target(java.lang.annotation.ElementType.METHOD)
 public @interface GenerateDiagram {
     String diagramName() default "";
