c2c14668907afde2f6587a972a0cf2b2c3098e4d
Daniel1464 <Daniel_Chen@caryacademy.org>
2026-05-30 12:40:29 -0400

    example code

---
+++ src/main/java/first/robot/Motor.java
@@ -0,0 +1,8 @@
+package first.robot;
+
+public class Motor {
+    // "software library"
+
+    // Vendordep - vendor dependency
+    // Regular software library
+}
--- src/main/java/first/robot/Robot.java
+++ src/main/java/first/robot/Robot.java
@@ -5,7 +5,9 @@
 package first.robot;
 
 import org.wpilib.command3.Scheduler;
+import org.wpilib.command3.button.CommandGamepad;
 import org.wpilib.framework.TimedRobot;
+
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -13,12 +15,18 @@ import org.wpilib.framework.TimedRobot;
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
+  private final CommandGamepad xbox = new CommandGamepad(0);
+  private final Arm arm = new Arm();
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   public Robot() {
+    xbox.rightBumper().whileTrue(arm.runArmAt7Volts());
   }
+
+  private void runBackgroundTasks() {}
 
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
